683847ddaf8ed4cf9913244875c1ba63622c98e6
yangshare <717449318@qq.com>
2024-03-25 22:28:42 +0800

    设置目录为当天日期

--- pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/general/GeneralPromptSample03.java
+++ pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/general/GeneralPromptSample03.java
@@ -6,6 +6,11 @@ import hiforce.pixel.open.client.comfy.PromptResult;
 import hiforce.pixel.open.client.comfy.SimpleComfyInvoker;
 import hiforce.pixel.open.sample.common.BaseLocalSample;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 /**
  * @author Rocky Yu
  * @since 2024/2/22
@@ -19,12 +24,20 @@ public class GeneralPromptSample03 extends BaseLocalSample {
 
     @Override
     public void execute() throws Exception {
+
+        // 格式化日期
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = LocalDate.now().format(formatter);
+
         String workflowJSON = getTextFromResource("/general/线框图/线框图.json");
         String workflowApiJSON = getTextFromResource("/general/线框图/线框图_api.json");
 
         WorkflowApi workflowApi = JSON.parseObject(workflowApiJSON, WorkflowApi.class);
-        workflowApi.setNodeFieldValue(6, "text",
-                "beautiful scenery nature glass bottle landscape, , purple galaxy bottle,");
+        workflowApi.setNodeFieldValue(30, "text",
+                "DOG");
+
+        workflowApi.setNodeFieldValue(26, "filename_prefix",
+                formattedDate + File.separator + "LineArt");
 
         SimpleComfyInvoker invoker = new SimpleComfyInvoker("http://192.168.6.2:8188");
         PromptResult promptResult = invoker.startQueuePrompt(workflowApi, workflowJSON);
