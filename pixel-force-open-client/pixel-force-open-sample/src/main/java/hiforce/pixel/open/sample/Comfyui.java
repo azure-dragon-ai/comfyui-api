81934166def88c05c1da66683b55da26c6a19db6
yangc <simayifeng@gmail.com>
2024-03-27 22:16:08 +0800

    增加新的workflow

--- pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/Comfyui.java
+++ pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/Comfyui.java
@@ -1,13 +1,32 @@
 package hiforce.pixel.open.sample;
 
+import com.alibaba.fastjson.JSON;
+import hiforce.pixel.comfy.model.node.WorkflowApi;
 import hiforce.pixel.open.sample.general.GeneralPromptSample03;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Comfyui {
+
+    // 格式化日期
+    static String formattedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
     public static void main(String[] args) {
         GeneralPromptSample03 sample = new GeneralPromptSample03();
-        String prompt = "rabbit";
+        /*String prompt = "car";
         String workflowJSON = sample.getTextFromResource("/general/线框图/线框图.json");
         String workflowApiJSON = sample.getTextFromResource("/general/线框图/线框图_api.json");
-        sample.init(prompt, workflowJSON, workflowApiJSON).run();
+        sample.init(prompt, workflowJSON, workflowApiJSON).run();*/
+
+
+        String prompt = "golden hour in beijing, dreamy chinese town,ground_vehicle, tree, motor_vehicle, scenery, motorcycle, east_asian_architecture, lantern, road, outdoors, architecture, paper_lantern, street, building, autumn_leaves, bicycle, autumn, sky, people";
+        String workflowJSON = sample.getTextFromResource("/general/花园/花园.json");
+        String workflowApiJSON = sample.getTextFromResource("/general/花园/花园_api.json");
+        WorkflowApi workflowApi = JSON.parseObject(workflowApiJSON, WorkflowApi.class);
+        workflowApi.setNodeFieldValue(6, "text", prompt);
+        workflowApi.setNodeFieldValue(9, "filename_prefix", formattedDate + File.separator + "LineArt");
+        sample.init(workflowJSON, workflowApi).run();
     }
 }
--- pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/general/GeneralPromptSample03.java
+++ pixel-force-open-client/pixel-force-open-sample/src/main/java/hiforce/pixel/open/sample/general/GeneralPromptSample03.java
@@ -6,45 +6,28 @@ import hiforce.pixel.open.client.comfy.PromptResult;
 import hiforce.pixel.open.client.comfy.SimpleComfyInvoker;
 import hiforce.pixel.open.sample.common.BaseLocalSample;
 
-import java.io.File;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 /**
  * @author Rocky Yu
  * @since 2024/2/22
  */
 public class GeneralPromptSample03 extends BaseLocalSample {
 
-    private String prompt;
-
     private String workflowJSON;
 
-    private String workflowApiJSON;
+    private WorkflowApi workflowApi;
+    ;
 
-    public BaseLocalSample init(String prompt, String workflowJSON, String workflowApiJSON) {
-        this.prompt = prompt;
+    public BaseLocalSample init(String workflowJSON, WorkflowApi workflowApi) {
         this.workflowJSON = workflowJSON;
-        this.workflowApiJSON = workflowApiJSON;
+        this.workflowApi = workflowApi;
         return this;
     }
 
     @Override
     public void execute() throws Exception {
 
-        // 格式化日期
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = LocalDate.now().format(formatter);
-
-        WorkflowApi workflowApi = JSON.parseObject(workflowApiJSON, WorkflowApi.class);
-        workflowApi.setNodeFieldValue(30, "text",
-                prompt);
-
-        workflowApi.setNodeFieldValue(26, "filename_prefix",
-                formattedDate + File.separator + "LineArt");
-
         SimpleComfyInvoker invoker = new SimpleComfyInvoker("http://192.168.6.2:8188");
+
         PromptResult promptResult = invoker.startQueuePrompt(workflowApi, workflowJSON);
 
         System.out.println("General Prompt invoke result:" + JSON.toJSONString(promptResult));
