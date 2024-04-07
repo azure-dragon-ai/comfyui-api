ecdc7559ed5f52f1b898503e62bccb502f463802
yangshare <717449318@qq.com>
2024-04-07 21:12:59 +0800

    提交uni框架依赖

--- ComfyUi-Server/ruoyi-ui/vue.config.js
+++ ComfyUi-Server/ruoyi-ui/vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.6.2:8080`,
+        target: `http://192.168.6.3:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
